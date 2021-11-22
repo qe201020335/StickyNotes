b76962ac5fdffb52d9fac93fa281c47305fc6ac9
Sky Z <qe201020335@sina.com>
2021-11-22 04:11:47 -0500

    basic note functionality

--- StickyNotes.js
+++ StickyNotes.js
@@ -8,16 +8,12 @@ class StickyNote {
     parentNode = null
     isPositionFixed = false
     noteDiv = document.createElement('div')
+    textField;
 
     constructor(isFixed, parent) {
         const newNote = this.noteDiv
 
         newNote.className = 'StickyNote'
-        newNote.style.width = '16%'
-        newNote.style.height = '9%'
-        newNote.style.borderRadius = '5px'
-        newNote.style.margin = '30px'
-        newNote.style.backgroundColor = 'grey'
 
         this.isPositionFixed = isFixed
         if (isFixed) {
@@ -30,7 +26,76 @@ class StickyNote {
             this.parentNode = parent
         }
 
-        newNote.innerText = "Place Holder Text"
+
+        const title_p = document.createElement('p')
+        title_p.innerText = "Sticky Note"
+        title_p.style.marginLeft = '10px'
+        newNote.append(title_p)
+
+        const btnx = document.createElement('bottom')
+        btnx.innerText = '×'
+        btnx.onclick = () => this.removeFromDom()
+        btnx.className = "noteCloseBtn"
+        title_p.append(btnx)
+
+
+        const btn_div = document.createElement('div')
+        btn_div.className = 'noteBtnDiv'
+
+        const btn1 = document.createElement('bottom')
+        btn1.innerText = '↖'
+        btn1.onclick = () => this.setLocationFixed('top-left')
+        btn1.className = "notePositionBtn"
+
+        const btn2 = document.createElement('bottom')
+        btn2.innerText = '↙'
+        btn2.onclick = () => this.setLocationFixed('bottom-left')
+        btn2.className = "notePositionBtn"
+
+        const btn3 = document.createElement('bottom')
+        btn3.innerText = '↗'
+        btn3.onclick = () => this.setLocationFixed('top-right')
+        btn3.className = "notePositionBtn"
+
+        const btn4 = document.createElement('bottom')
+        btn4.innerText = '↘'
+        btn4.onclick = () => this.setLocationFixed('bottom-right')
+        btn4.className = "notePositionBtn"
+
+        btn_div.append(btn1)
+        btn_div.append(btn2)
+        btn_div.append(btn3)
+        btn_div.append(btn4)
+
+        newNote.append(btn_div)
+
+        const textField = document.createElement('textarea')
+        textField.rows = 8
+        textField.cols = 40
+        textField.className = 'noteTextArea'
+        textField.wrap = 'hard'
+        newNote.append(textField)
+        this.textField = textField
+
+
+        const save_btn = document.createElement('button')
+        save_btn.innerText = 'Save'
+        save_btn.className = 'noteSaveBtn'
+        save_btn.onclick = () => this.saveNote()
+        newNote.append(save_btn)
+
+    }
+
+    saveNote() {
+        //console.dir(this.textField)
+        let element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(this.textField.value));
+        element.setAttribute('download', "Note" + new Date());
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
     }
 
     // setLocationAbsolute() {
@@ -43,6 +108,12 @@ class StickyNote {
         if (!this.isPositionFixed) {
             throw new Error("This note has absolute location");
         }
+
+        this.noteDiv.style.removeProperty('top')
+        this.noteDiv.style.removeProperty('left')
+        this.noteDiv.style.removeProperty('right')
+        this.noteDiv.style.removeProperty('bottom')
+
         switch (location) {
             case 'top-left':
                 this.noteDiv.style.top = '2%';
@@ -71,7 +142,7 @@ class StickyNote {
     }
 
     addToDOM() {
-        console.dir(this.parentNode)
+        // console.dir(this.parentNode)
         if (!this.parentNode.contains(this.noteDiv)) {
             this.parentNode.append(this.noteDiv)
         }
--- pub/example.js
+++ pub/example.js
@@ -9,28 +9,28 @@ let note4 = null;
 
 function onClickB1(location) {
     if (note1 === null) {
-        note1 = generator.createNoteFixed(location, document.querySelector(".content"));
+        note1 = generator.createNoteFixed(location, document.querySelector("#content"));
     }
     note1.addToDOM()
 }
 
 function onClickB2(location) {
     if (note2 === null) {
-        note2 = generator.createNoteFixed(location, document.querySelector(".content"));
+        note2 = generator.createNoteFixed(location, document.querySelector("#content"));
     }
     note2.addToDOM()
 }
 
 function onClickB3(location) {
     if (note3 === null) {
-        note3 = generator.createNoteFixed(location, document.querySelector(".content"));
+        note3 = generator.createNoteFixed(location, document.querySelector("#content"));
     }
     note3.addToDOM()
 }
 
 function onClickB4(location) {
     if (note4 === null) {
-        note4 = generator.createNoteFixed(location, document.querySelector(".content"));
+        note4 = generator.createNoteFixed(location, document.querySelector("#content"));
     }
     note4.addToDOM()
 }
